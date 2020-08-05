ffa6fab77bdbb88bfd4b4b0dba17df04d5c984a8
Siddhant2812 <68544278+Siddhant2812@users.noreply.github.com>
2020-08-05 15:56:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -38,8 +38,9 @@ function draw() {
 function hasCollided(bullet,wall){
   bulletRightEdge = bullet.x+bullet.width
   wallLeftEdge = wall.x;
-  if(bulletRightEdge>=wallLeftEdge){
+  if(bulletRightEdge>=wallLeftEdge)
+  {
     return true
-    return false
   }
+    return false;
 }
